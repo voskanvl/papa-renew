a488f39759e44656b7601b2cd1430536c1d6f164
Anton Voskanyan <voskanvl@gmail.com>
2021-06-23 23:03:27 +0300

    server

--- src/data/server.js
+++ src/data/server.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const path = require("path")
+const compression = require("compression")
 const PORT = process.env.PORT || 3000
 const app = express()
+app.use(compression())
 app.use(["/static/"], express.static(path.resolve("static")))
 app.use(["/"], express.static(path.resolve(__dirname)))
-app.get("http:localhost:3000/1.json", (req, res) => {
+app.get("/1.json", (req, res) => {
     res.sendFile(path.resolve(__dirname, "1.json"))
 })
 app.get(["/", "/:f", "/:f/:s"], (req, res) => {
